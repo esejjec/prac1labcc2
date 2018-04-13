ce4f3df447e1571823346744728047183eaa0817
esejjec <38010954+esejjec@users.noreply.github.com>
2018-04-12 19:20:45 -0500

    Update ejercicio1.cpp

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+using namespace std;
 
+void valref(int &p,int a){
+    cout << "por valor: " << a <<endl;
+    cout << "por referencia: " << p <<endl;
+}
+
+int main()
+{
+ int a = 5;
+ int &p = a;
+ valref(p,a);
+ return 0;
+}
